e16075d169a05ac19dcec30484aa36d2790f12b7
Amine Droua <aminus44@gmail.com>
2023-07-05 10:57:53 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,8 +12,8 @@ module.exports = {
       repo: "git@github.com:Username/Repo.git",
       ref: "origin/master",
       key: "key.pem", // SSH KEY
-      path: "/home/ci-cd/adjial",
-      "pre-deploy-local": "",
+      path: "/var/www/ci-cd/nextjs-flask-template",
+      "pre-deploy-local": "git add . && git commit -m 'Pushing to production' && git push origin master",
       "post-deploy": "source ~/.nvm/nvm.sh && npm install && npm run build && systemctl restart nginx.service && pm2 reload ecosystem.config.js --env production",// What ever you want to execute post-deploy
       "pre-setup": "",
       "ssh_options": "ForwardAgent=yes",
